4e2a8048ac74bb7227274947ae7b7bc5ccb82464
Kise Jin <20280093@student.hcmus.edu.vn>
2024-07-24 17:39:47 +0000

    new commit

--- alpha_dictionary_indicatos.py
+++ alpha_dictionary_indicatos.py
@@ -1,141 +1,213 @@
 indicators_dictionary = {
     "#": [
-            "52-Week High/Low",
-            "52-Week Range"
+                "52-Week High/Low",
+                "52-Week Range"
     ],
 
     "a": [
-            "Accumulation/Distribution Indicator (A/D)",
-            "Advance/Decline Line (A/D)",
-            "Aroon Indicator",
-            "Aroon Oscillator",
-            "Ascending Channel",
-            "Ascending Triangle",
-            "Average Directional Index (ADX)",
-            "Average True Range (ATR)"
+                "Accumulation/Distribution Indicator (A/D)",
+                "Advance/Decline Line (A/D)",
+                "Aroon Indicator",
+                "Aroon Oscillator",
+                "Ascending Channel",
+                "Ascending Triangle",
+                "Average Directional Index (ADX)",
+                "Average True Range (ATR)"
     ],
 
     "b":[
-            "Baltic Dry Index",
-            "Bearish Engulfing Pattern",
-            "Beta",
-            "Bollinger Band",
-            "Breadth Indicator",
-            "Breakout",
-            "Bullish Abandoned Baby",
-            "Bullish Engulfing Pattern",
-            "Bullish Harami",
-            "Butterfly Spread",
+                "Baltic Dry Index",
+                "Bearish Engulfing Pattern",
+                "Beta",
+                "Bollinger Band",
+                "Breadth Indicator",
+                "Breakout",
+                "Bullish Abandoned Baby",
+                "Bullish Engulfing Pattern",
+                "Bullish Harami",
+                "Butterfly Spread",
     ],
 
     "c": [
-            "Capital Market Line (CML)",
-            "Capitulation",
-            "Cash Conversion Cycle (CCC)",
-            "Channel Stuffing",
-            "Chart of Accounts (COA)",
-            "Cup and Handle Pattern",
+                "Capital Market Line (CML)",
+                "Capitulation",
+                "Cash Conversion Cycle (CCC)",
+                "Channel Stuffing",
+                "Chart of Accounts (COA)",
+                "Cup and Handle Pattern",
     ],
 
     "d": [
-            "Dark Cloud Cover",
-            "Darvas Box Theory",
-            "Dead Cat Bounce",
-            "Death Cross",
-            "Directional Movement Index (DMI)",
-            "Doji",
-            "Donchian Channels",
-            "Double Bottom",
-            "Double Exponential Moving Average (DEMA)",
-            "Double Top",
-            "Dragonfly Doji Candlestick",
-            "Dow Theory",
-            "Downtrend",
+                "Dark Cloud Cover",
+                "Darvas Box Theory",
+                "Dead Cat Bounce",
+                "Death Cross",
+                "Directional Movement Index (DMI)",
+                "Doji",
+                "Donchian Channels",
+                "Double Bottom",
+                "Double Exponential Moving Average (DEMA)",
+                "Double Top",
+                "Dragonfly Doji Candlestick",
+                "Dow Theory",
+                "Downtrend",
     ],
     "e": [
-
+                "Elliott Wave Theory",
+                "Envelope",
+                "Exponential Moving Average (EMA)",
+                "Evening Star",
     ],
 
     "f": [
-
+                "Fibonacci Extensions",
+                "Fibonacci Numbers and Lines",
+                "Fibonacci Retracement",
+                "Fisher Transform Indicator",
+                "Fractal Indicator",
     ],
 
     "g": [
-
+                "Gann Angles",
+                "Gann Fans",
+                "Gartley Pattern",
+                "Golden Cross",
+                "Gravestone Doji",
+                "Guppy Multiple Moving Average (GMMA)"
     ],
 
     "h": [
-
+                "Hammer Candlestick",
+                "Hanging Man Candlestick",
+                "Harami Cross",
+                "Head and Shoulders Pattern",
+                "Heikin-Ashi Technique",
+                "Hikkake Pattern",
+                "Histogram",
+                "Hook Reversal"
     ],
 
     "i": [
-
+                "Ichimoku Cloud",
+                "Ichimoku Kinko Hyo Indicator & Five Components Explained",
+                "Impulse Wave Pattern",
+                "Inside Day",
+                "Intraday Momentum Index (IMI)",
+                "Inverse Head And Shoulders"
     ], 
 
-    "j": [
-
-    ],
-
     "k": [
-
+                "Kagi Chart",
+                "Kairi Relative Index",
+                "Keltner Channel",
+                "Kicker Pattern",
+                "Kijun Line (Base Line)",
+                "Kijun-Sen (Base Line)",
+                "Klinger Oscillator",
+                "Know Sure Thing (KST)",
     ],
 
     "l": [
-
+                "Lagging Indicator",
+                "Linearly Weighted Moving Average (LWMA)",
+                "Logarithmic Price Scale",
+                "Long-Legged Doji",
+                "Low Volume Pullback"
     ],
 
     "m": [
-
+                "McClellan Oscillator",
+                "Mean Reversion",
+                "Momentum",
+                "Momentum Investing",
+                "Money Flow",
+                "Money Flow Index (MFI)",
+                "Morning Star",
+                "Moving Average (MA)",
+                "Moving Average Convergence Divergence (MACD)"
     ],
 
     "n": [
-
+                "Negative Directional Indicator (-DI)",
+                "Negative Volume Index (NVI)",
+                "Neckline",
+                "Moving Average (MA)"
     ],
 
     "o": [
-
+                "On-Balance Volume (OBV)",
+                "Oscillator",
+                "Oscillator of a Moving Average (OsMA)"
     ],
 
     "p": [
-
+                "Parabolic SAR (Stop and Reverse) Indicator",
+                "Pennant",
+                "Point-and-Figure (P&F) Chart",
+                "Price Action",
+                "Price Rate of Change Indicator (ROC)"
     ],
 
     "q": [
-
+                "Qstick Indicator"
     ],
 
     "r": [
-
+                "Rate of Change (ROC)",
+                "Relative Strength",
+                "Relative Strength Index (RSI)",
+                "Relative Vigor Index (RVI)",
+                "Renko Chart",
+                "Resistance (Resistance Level)",
+                "Retracement",
+                "Reversal"
     ],
 
     "s": [
-
+                "Simple Moving Average (SMA)",
+                "Stochastic Oscillator",
+                "Stochastic RSI (StochRSI)",
+                "Support (Support Level)"
     ],
     "t": [
-
+                "Tenkan-Sen (Conversion Line)",
+                "Three White Soldiers Candlestick Pattern",
+                "Trend",
+                "Trend Analysis",
+                "Trend Trading",
+                "Trendline",
+                "True Strength Index (TSI)",
+                "Triple Exponential Moving Average (TEMA)",
+                "Tweezer"
     ],
 
     "u": [
-
+                "Ulcer Index (UI)",
+                "Ultimate Oscillator",
+                "Up Volume",
+                "Upside Gap Two Crows",
+                "Upside Tasuki Gap",
+                "Uptick Volume",
+                "Uptrend"
     ],
 
     "v": [
-
+                "Volume Analysis",
+                "Volume Price Trend Indicator (VPT)",
+                "Volume Weighted Average Price (VWAP)",
+                "Vortex Indicator (VI)",
     ],
 
     "w": [
-
-    ],
-
-    "x": [
-
-    ],
-
-    "y": [
-
+                "Wilder's DMI (ADX)",
+                "Williams %R",
+                "Wolfe Wave",
+                "Worden Stochastics"
     ],
 
     "z": [
-
+                "Zig Zag Indicator",
+                "Zone of Resistance",
+                "Zone of Support"
     ]
 }
