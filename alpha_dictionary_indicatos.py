9491f385e8c561cf4ce9274dd7bf256df1ab8740
Kise Jin <20280093@student.hcmus.edu.vn>
2024-07-24 12:28:32 +0000

    new commit

---
+++ alpha_dictionary_indicatos.py
@@ -0,0 +1,141 @@
+indicators_dictionary = {
+    "#": [
+            "52-Week High/Low",
+            "52-Week Range"
+    ],
+
+    "a": [
+            "Accumulation/Distribution Indicator (A/D)",
+            "Advance/Decline Line (A/D)",
+            "Aroon Indicator",
+            "Aroon Oscillator",
+            "Ascending Channel",
+            "Ascending Triangle",
+            "Average Directional Index (ADX)",
+            "Average True Range (ATR)"
+    ],
+
+    "b":[
+            "Baltic Dry Index",
+            "Bearish Engulfing Pattern",
+            "Beta",
+            "Bollinger Band",
+            "Breadth Indicator",
+            "Breakout",
+            "Bullish Abandoned Baby",
+            "Bullish Engulfing Pattern",
+            "Bullish Harami",
+            "Butterfly Spread",
+    ],
+
+    "c": [
+            "Capital Market Line (CML)",
+            "Capitulation",
+            "Cash Conversion Cycle (CCC)",
+            "Channel Stuffing",
+            "Chart of Accounts (COA)",
+            "Cup and Handle Pattern",
+    ],
+
+    "d": [
+            "Dark Cloud Cover",
+            "Darvas Box Theory",
+            "Dead Cat Bounce",
+            "Death Cross",
+            "Directional Movement Index (DMI)",
+            "Doji",
+            "Donchian Channels",
+            "Double Bottom",
+            "Double Exponential Moving Average (DEMA)",
+            "Double Top",
+            "Dragonfly Doji Candlestick",
+            "Dow Theory",
+            "Downtrend",
+    ],
+    "e": [
+
+    ],
+
+    "f": [
+
+    ],
+
+    "g": [
+
+    ],
+
+    "h": [
+
+    ],
+
+    "i": [
+
+    ], 
+
+    "j": [
+
+    ],
+
+    "k": [
+
+    ],
+
+    "l": [
+
+    ],
+
+    "m": [
+
+    ],
+
+    "n": [
+
+    ],
+
+    "o": [
+
+    ],
+
+    "p": [
+
+    ],
+
+    "q": [
+
+    ],
+
+    "r": [
+
+    ],
+
+    "s": [
+
+    ],
+    "t": [
+
+    ],
+
+    "u": [
+
+    ],
+
+    "v": [
+
+    ],
+
+    "w": [
+
+    ],
+
+    "x": [
+
+    ],
+
+    "y": [
+
+    ],
+
+    "z": [
+
+    ]
+}
